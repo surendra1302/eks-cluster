407039b1dc8d8871c9d9a69d6dccb8d8ac842bf2
surendra1302 <surendrakorivi96@gmail.com>
2025-03-12 19:44:15 +0530

    Update main.tf

--- modules/nodegroup/main.tf
+++ modules/nodegroup/main.tf
@@ -17,8 +17,8 @@ resource "aws_eks_node_group" "eks_nodes" {
   subnet_ids = var.subnet_ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 5
+    max_size     = 15
     min_size     = 1
   }
 
