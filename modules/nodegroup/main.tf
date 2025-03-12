024d815afc6e171f7b26b37875126b6e80bf5b1a
surendra1302 <surendrakorivi96@gmail.com>
2025-03-12 19:43:44 +0530

    Update main.tf

---
+++ modules/nodegroup/main.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name = var.cluster_name
+  node_role_arn = aws_iam_role.eks_node_role.arn
+  subnet_ids = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+}
